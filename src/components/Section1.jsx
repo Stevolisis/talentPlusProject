7e80e873ff72ba7e0506c10999eb0c5d34458476
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-23 17:40:28 +0100

    buttons and previews

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -15,7 +15,7 @@ export default function Section1(){
                         <Link to='/' className="mx-3 text-txtHeaderLight">Courses</Link>
                         <Link to='/' className="mx-3 text-txtHeaderLight">Testimonial</Link>
                         <Link to='/' className="mx-3 text-txtHeaderLight">Community</Link>
-                        <Link to='/' className="ml-3 py-[12px] px-[28px] bg-neutral text-bgPrimary rounded-sm">Enroll Now</Link>
+                        <Link to='/' className="ml-3 py-[12px] px-[28px] bg-neutral text-bgPrimary rounded-[4px]">Enroll Now</Link>
                     </div>
                 </div>
 
@@ -28,8 +28,21 @@ export default function Section1(){
                             <p className="text-[13px] font-[SatoshiLight] text-[#E7E7E7]">Build Your Future With Our Quality Education. The Best And Largest All-In-One Online Tutoring Platform In The World</p>
                         </div>
                         <div className="py-3">
-                            <button className="mr-5 font-[SatoshiMedium] text-[13px] py-[10px] px-[28px] text-neutral border-2 border-txtHeaderLight rounded-md">Get Started Now</button>
-                            <button className="font-[SatoshiMedium] text-[13px] py-[12px] px-[28px] bg-neutral text-bgPrimary rounded-sm">Enroll Now</button>
+                            <button className="mr-5 font-[SatoshiMedium] text-[13px] py-[10px] px-[28px] text-neutral border-2 border-txtHeaderLight rounded-[4px]">Get Started Now</button>
+                            <button className="font-[SatoshiMedium] text-[13px] py-[12px] px-[28px] bg-neutral text-bgPrimary rounded-[4px]">Enroll Now</button>
+                        </div>
+                        <div className="flex items-center">
+                            <div className="flex items-center">
+                                <img className="w-[45px] h-[45px]" src='/images/preview1.png' alt='preview'/>
+                                <img className="w-[45px] h-[45px] ml-[-15px]" src='/images/preview2.png' alt='preview'/>
+                                <img className="w-[45px] h-[45px] ml-[-15px]" src='/images/preview3.png' alt='preview'/>
+                                <img className="w-[45px] h-[45px] ml-[-15px]" src='/images/preview4.png' alt='preview'/>
+                                <img className="w-[45px] h-[45px] ml-[-15px]" src='/images/preview5.png' alt='preview'/>
+                            </div>
+                            <div className="ml-1 flex justify-center items-center flex-col">
+                                <p className="text-[19px] font-[ClashDisplaySemibold] leading-none">255K+</p>
+                                <p className="text-[13px] font-[SatoshiLight] leading-none"> Previews</p>
+                            </div>
                         </div>
                     </div>
 

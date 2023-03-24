217ccf52a72423abaa828587017c44cde6f31d00
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-24 10:31:07 +0100

    sec2

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -4,7 +4,7 @@ export default function Section2(){
 
     return(
         <>
-            <div className="flex p-32 bg-bgWhole">
+            <div className="flex px-32 pt-20 pb-28 bg-bgWhole items-center">
                 <div className='flex-1'>
                     <div>
                         <p className='pr-12 font-[ClashDisplayBold] text-bgSecondary text-[2.7rem] leading-[55px]'>High quality video, audio & live classes</p>
@@ -15,15 +15,28 @@ export default function Section2(){
                     <div className='py-3'>
                         <button className='rounded-[4px] bg-bgPrimary font-[SatoshiMedium] py-[12px] px-[28px] text-[12px] text-neutral'>View Courses</button>
                     </div>
-                    <div>
-                        <div>
-                            <MdVolumeUp/>
+                    <div className='py-2 flex'>
+                        <div className='mr-4 rounded-[5px] flex items-center bg-neutral py-4 pl-2 pr-10 w-auto'>
+                            <p className='p-[6px] bg-bgCourseGreen rounded-[2px] mx-3'>
+                                <MdVolumeUp className='text-[12px] text-iconCourseGreen'/>
+                            </p>
+                            <p className='font-[SatoshiMedium] text-bgSecondary text-[13px]'>Audio Classes</p>
+                        </div>
+                        <div className='mr-4 rounded-[5px] flex items-center bg-neutral py-4 pl-2 pr-10 w-auto'>
+                            <p className='p-[6px] bg-bgCourseGreen rounded-[2px] mx-3'>
+                                <MdVolumeUp className='text-[12px] text-iconCourseGreen'/>
+                            </p>
+                            <p className='font-[SatoshiMedium] text-bgSecondary text-[13px]'>Live Classes</p>
                         </div>
                     </div>
                 </div>
 
-                <div className='flex-2'>
-                    <img src='/images/sec21.png' alt='section2'/>
+                <div className='flex-2 flex items-end pl-1'>
+                    <div><img className='w-[480px] h-[410px] min-w-[480px]' src='/images/sec21.png' alt='section21'/></div>
+                    <div className='absolute'>
+                        <img className='w-[220px] h-[62px] ml-[-70px]' src='/images/sec22.png' alt='section22'/>
+                        <img className='mt-[100px] ml-[-150px] mb-[-80px] w-[310px] min-w-[310px]  h-[200px]' src='/images/sec23.png' alt='section23'/>
+                    </div>
                 </div>
             </div>
         </>

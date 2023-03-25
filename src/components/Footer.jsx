caa86376b3d8367b7d3f2449ad129942b425baf4
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-25 19:28:04 +0100

    footer in progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,60 @@
 import { FaDiscord, FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Footer(){
 
     return( 
     <>
-        <footer className="bg-bgSecondary text-neutral px-24 py-28 flex justify-between">
+        <footer className="bg-bgSecondary text-neutral px-24 pt-28 pb-24 flex justify-between">
             <div>
                 <div>
-                    <h2 className="font-[AmazingKids] text-4xl text-neutral">Tech Time</h2>
-                    <p>Reach out to any one of our social media neworks</p>
+                    <h2 className="py-2 font-[AmazingKids] text-4xl text-neutral">Tech Time</h2>
+                    <p className="leading-6 text-[13px] font-[SatoshiLight]">Reach out to any one of our social media neworks</p>
                 </div>
-                <div>
-                    <FaFacebook/>
-                    <FaTwitter/>
-                    <FaYoutube/>
-                    <FaInstagram/>
-                    <FaDiscord/>
+                <div className="py-4 flex justify-between text-emailInputGrayIcons">
+                    <FaFacebook size={23}/>
+                    <FaTwitter size={23}/>
+                    <FaYoutube size={23}/>
+                    <FaInstagram size={23}/>
+                    <FaDiscord size={23}/>
                 </div>
             </div>
 
             <div>
                 <div>
-                    <h2>Useful Links</h2>
+                    <h2 className="font-[ClashDisplaySemiBold] text-2xl">Useful Links</h2>
                 </div>
-                <div>
-                    <p>Home</p>
-                    <p>About Us</p>
-                    <p>Our Courses</p>
-                    <p>Testimonials</p>
-                    <p>Our Community</p>
-                </div>
-            </div>
-
-            <div>
-                <div>
-                    <h2>Community</h2>
-                </div>
-                <div>
-                    <p>Help Centers</p>
-                    <p>Patners</p>
-                    <p>Suggestion</p>
-                    <p>Blog</p>
-                    <p>Newsletter</p>
+                <div className="py-5 font-[SatoshiLight] text-lg">
+                    <Link to='/' className="block my-2">Home</Link>
+                    <Link to='/' className="block my-2">About Us</Link>
+                    <Link to='/' className="block my-2">Our Courses</Link>
+                    <Link to='/' className="block my-2">Testimonials</Link>
+                    <Link to='/' className="block my-2">Our Community</Link>
                 </div>
             </div>
 
             <div>
                 <div>
-                    <h2>Subscribe Us</h2>
+                    <h2 className="font-[ClashDisplaySemiBold] text-2xl">Community</h2>
                 </div>
+                <div className="py-5 font-[SatoshiLight] text-lg">
+                    <Link className="block my-2" to='/'>Help Centers</Link>
+                    <Link className="block my-2" to='/'>Patners</Link>
+                    <Link className="block my-2" to='/'>Suggestion</Link>
+                    <Link className="block my-2" to='/'>Blog</Link>
+                    <Link className="block my-2" to='/'>Newsletter</Link>
+                </div>
+            </div>
+
+            <div>
                 <div>
-                    <input type='email' placeholder="Nft123@gmail.com"/>
-                    <button>Send Message</button>
+                    <h2 className="font-[ClashDisplaySemiBold] text-2xl">Subscribe Us</h2>
+                </div>
+                <div className="py-5 font-[SatoshiLight]">
+                    <input type='email' placeholder="Nft123@gmail.com" 
+                    className="border placeholder:[#424141] outline-none border-bgPrimary w-[213px] bg-emailInputGrayIcons 
+                    py-2 px-3 text-[11px]"/>
+                    <button className=" bg-bgPrimary py-[9px] px-3 text-[11px] rounded-sm">Send Message</button>
                 </div>
             </div>
         </footer>
